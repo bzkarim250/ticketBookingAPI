af0e0a0744aa74811eb2e9938c000d014d3c3252
Twizerimana12 <85844144+Twizerimana12@users.noreply.github.com>
2023-04-04 12:40:23 +0200

    ft-delete-bus successful

--- src/middlewares/validation.js
+++
@@ -1,9 +0,0 @@
-/* eslint-disable consistent-return */
-import out from '../helpers/response';
-
-const validate = (schema) => (req, res, next) => {
-  const { error } = schema.validate(req.body);
-  return error ? out(res, 422, error.message, null, 'VALIDATION_ERROR') : next();
-};
-
-export default validate;
